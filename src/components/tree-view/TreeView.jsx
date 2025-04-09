e15e3614754a8fda12da72581245bb1bd7408347
Psh <pyaesonehein81998@gmail.com>
2025-04-09 22:01:12 +0630

    [ADD] Tree Veiw components

--- src/components/tree-view/TreeView.jsx
+++ src/components/tree-view/TreeView.jsx
@@ -5,6 +5,7 @@ import "./TreeVIew.css";
 export default function TreeView({ menus = [] }) {
     return (
         <div className="tree-view-container">
+            <h2>Tree View</h2>
             <MenuList list={menus} />
         </div>
     );
